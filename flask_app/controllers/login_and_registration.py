c21324f3a3518998bfbdd20cdd81e2b054cd0865
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-02-21 00:21:18 -0500

    first commit

---
+++ flask_app/controllers/login_and_registration.py
@@ -0,0 +1,51 @@
+from flask import render_template, request, redirect, session, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+@app.route("/")
+def index():
+    return render_template("login_and_registration.html")
+
+
+@app.route("/new_user", methods=['POST'])
+def new_user():
+    if not User.validate_user(request.form):
+        return redirect("/")
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
+        "email": request.form["email"],
+        "password" : pw_hash,
+    }
+    user_id = User.create_user(data)
+    session['user_first_name'] = data["first_name"]
+    session['loged_in'] = True
+    return redirect ("/dashboard")
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = request.form
+    results = User.login(data)
+    if not results:
+        flash("Invalid Email/Password!",'login')
+        return redirect("/")
+    if not bcrypt.check_password_hash(results.password, request.form['password']):
+        flash("Invalid Email/Password!", 'login')
+        return redirect('/')
+    session['user_first_name'] = results.first_name
+    session['loged_in'] = True
+    return redirect("/dashboard")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html",first_name = session['user_first_name'])
+
+@app.route("/clear_session", methods=["POST"])
+def clear_session():
+    session.clear()
+    return redirect("/")
---
+++ flask_app/models/user.py
@@ -0,0 +1,59 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+import re
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+PASSWORD_REGEX = re.compile(r'^(?=.*\d)(?=.*[A-Z])[a-zA-Z\d]{8,45}$')
+
+
+
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+
+    @classmethod
+    def create_user(cls,data):
+        query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s ,NOW() , NOW())"
+        results = connectToMySQL('login_and_registration_schema').query_db( query, data )
+        return results
+
+    @classmethod
+    def login(cls,data):
+        query = "SELECT * from users WHERE email = %(email)s"
+        results = connectToMySQL('login_and_registration_schema').query_db( query, data )
+        if len(results) < 1:
+            return False
+        return User(results[0])
+
+
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True
+        if len(user['first_name']) < 3:
+            flash("First name must be at least 2 characters.", 'create_user')
+            is_valid = False
+        if len(user['last_name']) < 3:
+            flash("Last name must be at least 2 characters.", 'create_user')
+            is_valid = False
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", 'create_user')
+            is_valid = False
+        if User.login(user) != False:
+            flash("Email address already exist!", 'create_user')
+            is_valid = False
+        if not PASSWORD_REGEX.match(user['password']):
+            flash("Passwords must have a least 1 number and 1 uppercase letter and minimum of 8 digits length", 'create_user')
+            is_valid = False
+        if user['password'] != user['confirm_password']:
+            flash("Password and Confirm Password has to be the same", 'create_user')
+            is_valid = False
+        return is_valid
